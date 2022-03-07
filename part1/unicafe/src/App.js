0572e4a3fdd9e38c1fcf8c2f3984a0e1d4a85557
Mohammad <moh.osama84x@gmail.com>
2022-03-07 17:39:59 +0200

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,11 +9,20 @@ const Button = (props) => {
   )
 }
 
-const Statistics =(props)=>{
- return (
-  <p>{props.text}  {props.value}  {props.text==="positive" && " %"} </p>
+const Statistics = (props) => {
+  return (
+    <>
+      <tr>
+        <td>
+          {props.text}
+        </td>
 
- )
+        <td>
+          {props.value}  {props.text === "positive" && " %"}
+        </td>
+      </tr>
+    </>
+  )
 
 }
 
@@ -35,23 +44,23 @@ const App = () => {
   }
 
 
-  const total=()=>{
-   let total =good+neutral+bad
+  const total = () => {
+    let total = good + neutral + bad
     return total
   }
 
-  const average=(total)=>{
-    let average =(good-bad) /total
-     return average
-   }
-   const positive=(total)=>{
-    let positive =(good/total) *100
-     return positive
-   }
+  const average = (total) => {
+    let average = (good - bad) / total
+    return average
+  }
+  const positive = (total) => {
+    let positive = (good / total) * 100
+    return positive
+  }
 
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div  >
 
       <div>
         <h1>give feedback</h1>
@@ -66,19 +75,27 @@ const App = () => {
         <div>
           <h1>Statistics</h1>
         </div>
+
         <div>
-        <Statistics value={good} text="good" />
-        <Statistics value={neutral} text="neutral" />
-        <Statistics value={bad} text="bad" />
+          <table>
+            <tbody>
+              <Statistics value={good} text="good" />
+              <Statistics value={neutral} text="neutral" />
+              <Statistics value={bad} text="bad" />
+            </tbody>
+          </table>
           <br></br>
           {!total() & !average(total()) & !positive(total())
-             ? <p>no feedback given</p>
-             : [ <Statistics value={total()} text="total" /> ,
-                <Statistics value={average(total())} text="average" /> ,
-                <Statistics value={positive(total())} text="positive" /> ] 
-                                }
-          
-          
+            ? <p>no feedback given</p>
+            : <table>
+              <tbody>
+                <Statistics value={total()} text="total" />
+                <Statistics value={average(total())} text="average" />
+                <Statistics value={positive(total())} text="positive" />
+              </tbody>
+            </table>
+          }
+
         </div>
 
       </div>
