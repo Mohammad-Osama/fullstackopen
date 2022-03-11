3df1eab8bf34ce1ef4d3f42338716464aaba6f61
Mohammad <moh.osama84x@gmail.com>
2022-03-11 23:40:02 +0200

    2.14*: Data for countries, step3

--- part2/countries/src/components/CountryDetailed.js
+++ part2/countries/src/components/CountryDetailed.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect ,useState } from 'react'
+
 
 export default function CountryDetailed(props) {
    // console.log("single props", props)
+   const [weather, setWeather] = useState()
+
+
+
+   useEffect(() => {
+       const name = props.details.capital
+       const api_key = process.env.REACT_APP_API_KEY
+       axios
+       .get(`http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${api_key}&units=metric`)
+       .then(res => {
+        setWeather(res.data)
+        console.log('weather data', res.data)
+           } )
+       
+   }, [])
+
+   if (weather===undefined)
+     return (
+          <div>Loading...</div>
+               )
+       else        
     return (
         <>
             <div>
@@ -27,6 +51,12 @@ export default function CountryDetailed(props) {
             </div>
             <div>
                 <img src={props.details.flags.png} />
+            </div>
+            <div>
+                <h3>Weather in {props.details.capital}</h3>
+                <p>temperature : {weather.main.temp}  C </p>
+                <img  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+                <p>Wind: {weather.wind.speed} km/h , {weather.wind.deg} deg </p>
             </div>
 
 
